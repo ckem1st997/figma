0fdd4547208ddf50ce86a9321a2fae54f6bc0643
NGUYEN KHA HOP <1721050681@student.humg.edu.vn>
2021-01-17 15:57:55 +0700

    17/1/2021 2

---
+++ Migrations/20210117085510_mem.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace figma.Migrations
+{
+    public partial class mem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmEmail",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "token",
+                table: "Members");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ConfirmEmail",
+                table: "Members",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "token",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ Migrations/20210117085646_mem2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace figma.Migrations
+{
+    public partial class mem2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ConfirmEmail",
+                table: "Members",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "token",
+                table: "Members",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmEmail",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "token",
+                table: "Members");
+        }
+    }
+}
