aed688522c2c218654aa5eef36720c7e360c4f9f
NGUYEN KHA HOP <1721050681@student.humg.edu.vn>
2021-02-06 14:02:42 +0700

    6/2/2021 2

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -38,58 +38,49 @@ namespace figma.Controllers
             if (filesadd == null || filesadd.Count == 0)
                 return Ok(new { imgNode = "Không có hình ảnh được chọn", result = false });
             var filePaths = new List<string>();
-            string sql = "", notica = "";
+            string sql = "";
             foreach (var formFile in filesadd)
             {
-                if (FormFileExtensions.IsImage(formFile))
+                if (FormFileExtensions.IsImage(formFile) && formFile.Length > 0 && formFile.Length <= 4096000)
                 {
-                    if (formFile.Length > 0 && formFile.Length <= 4096000)
+                    var filePath = Path.Combine(_hostingEnvironment.WebRootPath, "" + path + "");
+                    filePaths.Add(filePath);
+                    var randomname = DateTime.Now.ToFileTime() + Path.GetExtension(formFile.FileName);
+                    var fileNameWithPath = string.Concat(filePath, "\\", randomname);
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
                     {
-                        var filePath = Path.Combine(_hostingEnvironment.WebRootPath, "" + path + "");
-                        filePaths.Add(filePath);
-                        var randomname = DateTime.Now.ToFileTime() + Path.GetExtension(formFile.FileName);
-                        var fileNameWithPath = string.Concat(filePath, "\\", randomname);
-                        using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
-                        {
-                            await formFile.CopyToAsync(stream);
-                        }
-                        Resize(fileNameWithPath, width > 0 ? width : 1200, height > 0 ? height : 900);
-                        //FileStream ms;
-                        //ms = new FileStream(fileNameWithPath, FileMode.Open);
-                        //var auth = new FirebaseAuthProvider(new FirebaseConfig(_firebase.ApiKey));
-                        //var a = await auth.SignInWithEmailAndPasswordAsync(_firebase.AuthEmail, _firebase.AuthPassword);
-                        //var cancellation = new CancellationTokenSource();
-                        //var task = new FirebaseStorage(
-                        //    _firebase.Bucket,
-                        //    new FirebaseStorageOptions
-                        //    {
-                        //        AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
-                        //        ThrowOnCancel = true
-                        //    })
-                        //    .Child($"uploads/" + createFolderDate + "/" + randomname + "")
-                        //    .PutAsync(ms, cancellation.Token);
-
-                        //task.Progress.ProgressChanged += (s, e) => Console.WriteLine($"Progress: {e.Percentage} %");
-                        // ms.Close();
-                        if (sql.Length > 1)
-                            sql = "" + sql + ",uploads/" + createFolderDate + "/" + randomname + "";
-                        else
-                            sql = "uploads/" + createFolderDate + "/" + randomname + "";
-
+                        await formFile.CopyToAsync(stream);
                     }
+                    Resize(fileNameWithPath, width > 0 ? width : 1200, height > 0 ? height : 900);
+                    //FileStream ms;
+                    //ms = new FileStream(fileNameWithPath, FileMode.Open);
+                    //var auth = new FirebaseAuthProvider(new FirebaseConfig(_firebase.ApiKey));
+                    //var a = await auth.SignInWithEmailAndPasswordAsync(_firebase.AuthEmail, _firebase.AuthPassword);
+                    //var cancellation = new CancellationTokenSource();
+                    //var task = new FirebaseStorage(
+                    //    _firebase.Bucket,
+                    //    new FirebaseStorageOptions
+                    //    {
+                    //        AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
+                    //        ThrowOnCancel = true
+                    //    })
+                    //    .Child($"uploads/" + createFolderDate + "/" + randomname + "")
+                    //    .PutAsync(ms, cancellation.Token);
+
+                    //task.Progress.ProgressChanged += (s, e) => Console.WriteLine($"Progress: {e.Percentage} %");
+                    // ms.Close();
+                    if (sql.Length > 1)
+                        sql = "" + sql + ",uploads/" + createFolderDate + "/" + randomname + "";
                     else
-                    {
-                        return Ok(new { imgNode = "File được chọn phải có kích thước > 0 và <4M !", result = false });
-                    }
+                        sql = "uploads/" + createFolderDate + "/" + randomname + "";
                 }
                 else
-                    return Ok(new { imgNode = "File được chọn có định dạng không phải là hình ảnh", result = false });
+                    return Ok(new { imgNode = "File được chọn có định dạng không phải là hình ảnh hoặc file được chọn phải có kích thước > 0 và <4M !", result = false });
             }
             return Ok(new
             {
                 imgNode = sql,
-                result = true,
-                notica
+                result = true
             });
         }
 
